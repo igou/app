a15c1ec3172ba6e0cad2ca3052cbbf477c6b06bc
igou <734368243@qq.com>
2016-01-05 23:18:55 +0800

    ss

--- etc/char.c
+++ etc/char.c
@@ -45,11 +45,98 @@ int _str_reverse(char * s)
 	printf("\n"); 
    return 0;
 }
+/*统计字母出现频率 
+ */
+int alphabet_fre(const char *str)
+{
+#define LEN 26
+	int i=0;
+	char cmp[LEN]={0},ch;
+	
+	if(str == NULL)
+		return -1;
+	ch = str[i];
+	while(ch){
+		cmp[ch-'a']++;
+		i++;
+		ch = str[i]; 
+	}
+	for(i=0;i<LEN;i++)
+	{
+		if(cmp[i]>0)
+			printf("%c:%d ",'a'+i,cmp[i]);
+	}
+	printf("\n");
+#undef LEN
+}
+//TODO:多个字符长度相等且为最大的情况 
+int alphabet_longest(const char *str)
+{
+	char lgtch;
+	int i=1,l=0;
+	int  lgt=0,count=0;
+	if(str == NULL)
+		return -1;
+
+	if(str[0] == 0)
+          return 0;
+   // here: 至少有一个字符 
+	lgtch = str[0];
+	lgt = 1;
+	while(str[i++]){//比较前后字符，注意通过手段判断两个位置都是有效字符 ; 
+		if(!(str[i-1]^str[i])){
+	         count++;		
+	         }
+        else{
+            if(count>=lgt)
+              {
+                 lgt =count;
+                 lgtch = str[i-1];
+              }
+            count = 1;       
+        }
+  }
+  	printf("%c:%d \n",lgtch,lgt);
+} 
+//两个for循环，打印*或space通过条件语句判断(通过*的index范围来判断) 
+int  print_star(void)
+{
+    int i,j;
+    #define T 3
+    #if 1
+    #define N 3 
+    #else
+    #define N (2*T-1)
+    #endif
+    for(i=0;i<N/2+1;i++){
+        for(j=0;j<N;j++){
+            if(j>=((N>>1)-i)&& j<=((N>>1)+i))  // N/2就是最中间位置的index 
+                printf("*");
+            else
+                printf(" ");
+        }
+    printf("\n");
+    }    
+    for(i=0;i<N/2;i++){
+        for(j=0;j<N;j++){
+            if(j>i&& j<N-1-i)  //  
+                printf("*");
+            else
+                printf(" ");
+        }
+    printf("\n");
+    }    
+}
+
 int main(int argc, char *argv[]) {
  char str[]="abcdef";
- _str_reverse(str); /*Fix: the str should be modifiable==>
+
+ alphabet_fre("helllloworlkd");
+ alphabet_longest("helloowoooorldddddddddddd");
+ //_str_reverse(str);
+  /*Fix: the str should be modifiable==>
                       _str_reverse("abcdef");Error:it will occur 
 		      seg fault if modify data of "acdef"
 		    */
- #endif
+ print_star();
 }
--- etc/list.c
+++ etc/list.c
@@ -1,79 +1,146 @@
 #include<stdio.h> 
 #include<stdlib.h>
 
-typedef struct __node{
-struct {
-int value;
-struct __node * next;
-};
-}node;
 
-static node *sl_head;
+typedef struct node{
+    struct node *next;
+    struct node *prev;    
+    union {
+        int  num;
+        char alpha; 
+        void * p;
+    }data;
+}node_t;
+int list_init(node_t *node)
+{
+    node->next = node;
+    node->prev = node;
+}
+int list_add(node_t *head,node_t *node)
+{
+    node->next = head->next;
+    node->prev = head;
+    head->next->prev = node;
+    head->next = node;    
+}
+int list_add_tail(node_t *head,node_t *node)
+{
+    node->next = head;
+    node->prev = head->prev;
+    head->prev->next = node;
+    head->prev = node;    
+}
+int print_list(node_t *head)
+{
+    node_t *node;
+   /* 此处遍历链表时，head头只是作为一个入口点，其中不包括有意义的数据 ,
+     *否则在遍历的条件处理上就没有这么简单
+     */ 
+    for(node = head->next;node != head;node=node->next){
+         printf("%c ",node->data.alpha);        
+    }
+    printf("\n");
+}
+int list_reverse(node_t *head) 
+{
+    node_t *cur,*prev,*next; // miss *
+    for(prev = head,next = NULL,cur = head->next;cur!= head;)//error:prev = NULL
+    {
+        next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;     
+    }
+    cur->next = prev;//循环链表的反转，要多出个head特殊处理
+}
+int build_list(node_t *head)
+{  
+   char ch=0;
+   node_t *node=NULL;
+   
+   printf("input\n");
+   
+   while((ch=getchar())!=EOF){
+       getchar();
+       node=(node_t *)malloc(sizeof(node_t));
+       if(node != NULL){
+               node->data.alpha = ch;
+                list_add_tail(head,node); // head not init 
+       }        
+   } 
+   print_list(head);
+   list_reverse(head);
+   print_list(head);
+}
+/*-------------------------------单链表实现----------------------------------------------*/ 
+
+static node_t *sl_head;
 void main_start(void)
 {
 	printf("macro test\n");
 }
-void init_node(node **p)
+void single_init_node(node_t **p)
 {
-	(*p)=(node *)malloc(sizeof(node));
+	(*p)=(node_t *)malloc(sizeof(node_t));
 	(*p)->next = NULL; //Notice 循环单链表 
-	(*p)->value = ~0L;
+	(*p)->data.num = ~0L;
 }
-node * node_new(void)
+node_t * node_new(void)
 {
-	return (node *)malloc(sizeof(node));
+	return (node_t *)malloc(sizeof(node_t));
 }
-node * node_alloc(int v)
+node_t * node_alloc(int v)
 {
-	node *p=NULL;
+	node_t *p=NULL;
 	p = node_new();
 	if(p != NULL)
-		p->value = v;
+		p->data.num = v;
 	return p;
 }
-void node_free(node *n)
+void node_free(node_t *n)
 {
 	if(n != NULL)
 		free(n);
 } 
-void __node_add(node *prev,node *next)
+//single linked list
+void __single_node_add(node_t *prev,node_t *next)
 {
 	next->next = prev->next; //若prev为单链表最后一个，这里不检查其next域是否为null，调用者自己保证。 
 	prev->next = next;
 }
-void __node_del(node *prev,node *node)
+void __single_node_del(node_t *prev,node_t *node)
 {
 	prev->next = node->next; //若next为单链表最后一个，这里不检查其next域是否为null，调用者自己保证。 
 }
 /*	stack */
-void  node_add(node *head,node * new) 
+void  single_node_add(node_t *head,node_t * new) 
 {
-	__node_add(head,new);
+	__single_node_add(head,new);
 }
 /*  queue*/
-void  node_add_tail(node *head,node * new) 
+void  single_node_add_tail(node_t *head,node_t * new) 
 {
 			
 }
-void node_insert(node *head,node *new)
+void single_node_insert(node_t *head,node_t *new)
 {
-	__node_add(head,new);	
+	__single_node_add(head,new);	
 }
-int node_del(node *prev,node *node)
+int single_node_del(node_t *prev,node_t *node)
 {
-	__node_del(prev,node); 
+	__single_node_del(prev,node); 
 }
 
 /*   platform */
-int add_node(int v,node *new)
+int single_add_node(int v,node_t *new)
 {
-	node * p;
+	node_t * p;
 	if (new == NULL)
 	  return 1;
 	
 	for(p = sl_head;p;p=p->next){
-			if(v == p->value){
-				node_add(p,new);
+			if(v == p->data.num){
+				single_node_add(p,new);
 				return 0;
 			 }
 	}	
@@ -82,23 +149,23 @@ int add_node(int v,node *new)
 int del_node(int v)
 {
 // todo ：使用二级指针可以不用维护prev指针 
-  node *p,*prev=NULL;
+  node_t *p,*prev=NULL;
   /*case: head is matching*/
   for(p = sl_head;p;prev = p,p=p->next){
-  	   if(v == p->value && p != sl_head){
-  	   	 node_del(prev,p);
+  	   if(v == p->data.num && p != sl_head){
+  	   	 single_node_del(prev,p);
   	   	 return 0;
 		 }
   }
    return 1;//error
 }
-// 二级指针示例 
+// 二级指针应用于单链表示例 
 int del_nodes(int v)
 {
-	node **p=&sl_head;
+	node_t **p=&sl_head;
 	for(;(*p)->next;p=&(*p)->next){
-		if(v == (*p)->next->value){
-			node_del(*p,(*p)->next);//对于单向链表的话，删除最后一个时，(*p)->next会被赋值为NULL,执行下一句 p=&(*p)->next会出错(取NULL的地址) ；当然循环单链表的话就OK 
+		if(v == (*p)->next->data.num){
+			single_node_del(*p,(*p)->next);//对于单向链表的话，删除最后一个时，(*p)->next会被赋值为NULL,执行下一句 p=&(*p)->next会出错(取NULL的地址) ；当然循环单链表的话就OK 
 			return 0;// it can fix the issue above:skip p=&(*p)->next
 		}
 	}
@@ -107,31 +174,37 @@ int del_nodes(int v)
 int main(void) 
 {
 	int i;
-	node * pos,*new,*tmp;
-	init_node(&sl_head);//必须传入二级指针，才能给全局变量sl_head赋值
+	node_t * pos,*new,*tmp;
+	single_init_node(&sl_head);//必须传入二级指针，才能给全局变量sl_head赋值
 	for(i=0;i<6;i++){
-		new = (node *)node_alloc(5-i);
-		node_add(sl_head,new);
+		new = (node_t *)node_alloc(5-i);
+		single_node_add(sl_head,new);
 	}
 	for(pos=sl_head->next;pos;pos=pos->next)
-		printf("value:%d\n",pos->value);
+		printf("value:%d\n",pos->data.num);
 	del_nodes(4);
-	add_node(0,node_alloc(1003));	
+	single_add_node(0,node_alloc(1003));	
 	del_nodes(1);
 	del_nodes(5);
 	printf("------------------------\n");
 	for(pos=sl_head->next;pos;pos=pos->next)
-		printf("value:%d\n",pos->value);
+		printf("value:%d\n",pos->data.num);
 	printf("step1\n");	
 		for(pos=sl_head->next;pos;pos=tmp){
-			  node_del(sl_head,pos);
+			  single_node_del(sl_head,pos);
 			  tmp = pos->next;//notice :before free,recored next field
 			   node_free(pos); 
 	}
 		
 	printf("ok\n");
 	for(pos=sl_head->next;pos;pos=pos->next)
-		printf("value:%d\n",pos->value);
+		printf("value:%d\n",pos->data.num);
 	printf("step2\n");
 	node_free(sl_head); //fix:sl_head NOT free
+	
+	/* { 
+         node_t head={0};
+        list_init(&head);
+        build_list(&head);}
+    */ 
 } 
