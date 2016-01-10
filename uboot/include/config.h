a8e48b17eb114649bf330c1f561ea45c659cb2f1
igou <734368243@qq.com>
2016-01-10 20:36:09 +0800

    ubooot

---
+++ uboot/include/config.h
@@ -0,0 +1,13 @@
+/* Automatically generated - do not edit */
+#define CONFIG_SYS_ARCH  "arm"
+#define CONFIG_SYS_CPU   "arm920t"
+#define CONFIG_SYS_BOARD "smdk2410"
+#define CONFIG_SYS_VENDOR "samsung"
+#define CONFIG_SYS_SOC    "s3c24x0"
+#define CONFIG_BOARDDIR board/samsung/smdk2410
+#include <config_cmd_defaults.h>
+#include <config_defaults.h>
+#include <configs/smdk2410.h>
+#include <asm/config.h>
+#include <config_fallbacks.h>
+#include <config_uncmd_spl.h>
---
+++ uboot/include/config.mk
@@ -0,0 +1,5 @@
+ARCH   = arm
+CPU    = arm920t
+BOARD  = smdk2410
+VENDOR = samsung
+SOC    = s3c24x0
